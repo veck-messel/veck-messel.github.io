9af017998ace8d9eacaab4bb68de01b3665a5582
ArturPimenov <aptyp2001@mail.ru>
2024-03-15 19:30:39 +0300

    finish case and partner blocks and add new fonts

--- src/js/load_sections/index_load_sections.js
+++ src/js/load_sections/index_load_sections.js
@@ -1,12 +1,14 @@
 const indexSections = new Map([
     ['.hidden_menu', 'hidden_menu.html'],
     ['.header', 'header.html'],
+    ['.section1', 'preview.html'],
     ['.section2', 'about.html'],
     ['.section3', 'principles.html'],
     ['.section4', 'technology.html'],
     ['.section5', 'cases.html'],
     ['.section6', 'partners.html'],
     ['.section7', 'conversation_form.html'],
+    ['.footer', 'footer.html'],
 ]);
 
 $(document).ready(() => {
